a92cec2647a77a9409c9f8de1581be0d07fc1b6c
VohraAK <abdullah.k.vohra@gmail.com>
2024-05-14 15:12:49 +0500

    connect to MongoDB

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,24 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config();
 
 const app = express();
 
 app.listen(3000, () => {
-    console.log("Server is running on port 3000")
+    console.log(`Server is running on port 3000`)
 });
+
+const connectDB = async () => {
+    try 
+    {
+        await mongoose.connect(`${process.env.MONGO}`)
+        console.log("Connected to Database!")
+    } 
+    catch (error) 
+    {
+        console.log(error)
+    }
+};
+
+connectDB();
