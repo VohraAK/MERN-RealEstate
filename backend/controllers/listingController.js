c5b81bffa150519b3a75db6ddcbfcc2d0f8b2b01
VohraAK <abdullah.k.vohra@gmail.com>
2024-05-28 14:35:05 +0500

    create update listing API route

--- backend/controllers/listingController.js
+++ backend/controllers/listingController.js
@@ -11,7 +11,8 @@ export const createListing = async (request, response, next) =>
     catch (error) { next(error) };
 };
 
-export const deleteListing = async (request, response, next) => {
+export const deleteListing = async (request, response, next) => 
+{
     try 
     {
         const listing = await Listing.findById(request.params.id);
@@ -26,4 +27,20 @@ export const deleteListing = async (request, response, next) => {
 
     } 
     catch (error) { next(error); }
+};
+
+export const updateListing = async (request, response, next) => 
+{
+    try
+    {
+        const listing = await Listing.findById(request.params.id);
+        if (!listing) return next(errorHandler(401, "Listing not found!"));
+        
+        // check if user is authenticated to update listing
+        if (request.user.id !== listing.userRef) return next(errorHandler(401, "You can only update your own listings!"));
+
+        const updatedListing = await Listing.findByIdAndUpdate(request.params.id, request.body, { new: true });
+        response.status(200).json(updatedListing);
+    }
+    catch (error) { next(error) };
 };
--- backend/routes/listingRoute.js
+++ backend/routes/listingRoute.js
@@ -1,10 +1,11 @@
 import express from 'express';
-import { createListing, deleteListing } from '../controllers/listingController.js';
+import { createListing, deleteListing, updateListing} from '../controllers/listingController.js';
 import { verifyUser } from "../utils/verifyUser.js"
 
 const listingRouter = express.Router();
 
 listingRouter.post('/create', verifyUser, createListing);
-listingRouter.delete('/delete/:id', verifyUser, deleteListing)
+listingRouter.delete('/delete/:id', verifyUser, deleteListing);
+listingRouter.post('/update/:id', verifyUser, updateListing);
 
 export default listingRouter;
