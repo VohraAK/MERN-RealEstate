ab97ca5d66d1e2063600efea58e209fc371638e8
VohraAK <abdullah.k.vohra@gmail.com>
2024-06-04 17:15:51 +0500

    complete header search form functionality

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,9 +1,34 @@
 import { FaSearch } from 'react-icons/fa';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import { useSelector } from 'react-redux';
+import { useState, useEffect } from 'react';
 
 export default function Header() {
   const { currentUser } = useSelector((state) => state.user);
+  const [searchTerm, setSearchTerm] = useState("");
+  const navigate = useNavigate();
+  console.log(searchTerm);
+
+
+  const handleSearchSubmit = (e) => {
+    e.preventDefault();
+
+    // get previous params
+    const urlParams = new URLSearchParams(window.location.search);
+    urlParams.set('searchTerm', searchTerm);
+    const searchQuery = urlParams.toString();
+    navigate(`/search?${searchQuery}`);
+  };
+
+  useEffect(() => {
+    const urlParams = new URLSearchParams(location.search);
+    const searchTermFromURL = urlParams.get('searchTerm');
+    if (searchTermFromURL) {
+      setSearchTerm(searchTermFromURL);
+    } 
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [location.search]);
+
   return (
     <header className='bg-gray-200 shadow-md'>
       <div className='flex justify-between items-center mx-auto p-3.5 md:pd-3'>
@@ -15,9 +40,10 @@ export default function Header() {
           </button>
         </h1>
         </Link>
-        <form className='bg-slate-100 rounded-md flex items-center p-2'>
-            <input className='bg-transparent text-lg focus:outline-none w-24 sm:w-60 sm:text-sm' type='text' placeholder='Search...' />
-            <FaSearch className='text-slate-500 mx-1.5'/>
+        <form onSubmit={handleSearchSubmit} className='bg-slate-100 rounded-md flex items-center p-2'>
+            <input value={searchTerm} onChange={(e) => {setSearchTerm(e.target.value)}} className='bg-transparent text-lg focus:outline-none w-24 sm:w-60 sm:text-sm' type='text' placeholder='Search...' />
+            <button><FaSearch className='text-slate-500 mx-1.5'/></button>
+            
         </form>
         <ul className='flex gap-5 items-center'>
           <Link to='/about'><li className='hover:text-slate-600 hover:cursor-pointer text-center'>About</li></Link>
