2904c281dd3a8227477f60363fec3dbe7e8a9b8b
VohraAK <abdullah.k.vohra@gmail.com>
2024-06-12 20:06:09 +0500

    create about page

--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -1,7 +1,8 @@
-import React from 'react'
-
 export default function About() {
   return (
-    <div>About</div>
+    <div>
+      <h1 className='font-semibold text-3xl p-3'>About Ghar Dhoondho</h1>
+      <span className="">Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga atque blanditiis quod voluptatum magnam sunt sit adipisci! Quisquam voluptatum quas ab consequatur officiis dignissimos</span>
+    </div>
   )
 }
